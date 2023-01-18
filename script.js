3fe3e797c0118b63ffdf99c74cf2876832ef0903
Vaidikramani0509 <105126631+Vaidikramani0509@users.noreply.github.com>
2023-01-18 15:15:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        // let navigation = document.querySelector('.navigation');
+        // navigation.onclick = function () {
+        //     navigation.classlist.toggle('active')
+        // }
+    let navigation = document.querySelector('.navigation');
+    navigation.onclick = function () {
+        navigation.classList.toggle('active')
+    }
